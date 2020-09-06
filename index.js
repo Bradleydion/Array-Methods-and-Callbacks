ef63fb9577b23b065a90a1eeaefb19f970de1ba3
Bradleydion <57144164+Bradleydion@users.noreply.github.com>
2020-09-06 13:52:26 -0400

    completed the first task, but want to revisit because I think there is another way to call it and would like to learn it

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ console.log('its working');
 /* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data */
 
 // (a) Home Team name for 2014 world cup final
-console.log(fifaData[850]["Home Team Name"])
+// console.log(fifaData["2014"]["Final"]["Home Team Name"])
+console.log(fifaData[828]["Home Team Name"])
 // (b) Away Team name for 2014 world cup final
-
+console.log(fifaData[828]["Away Team Name"])
 // (c) Home Team goals for 2014 world cup final
-
+console.log(fifaData[828]["Home Team Goals"])
 // (d) Away Team goals for 2014 world cup final
-
+console.log(fifaData[828]["Away Team Goals"])
 // (e) Winner of 2014 world cup final 
-
+console.log(fifaData[828]["Win conditions"])
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
