449390bf243314e1460e97cea413110cc4949ddf
Bradleydion <57144164+Bradleydion@users.noreply.github.com>
2020-09-06 20:40:09 -0400

    cannot figure out task 2 still working on it

--- index.js
+++ index.js
@@ -20,17 +20,29 @@ console.log(fifaData[828]["Win conditions"])
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(data) {
-    const theFinalist = []
-  for(let i =0; i <= data.length; i++){
-  if(data[i].Stage[""]=== data.Stage["Final"]){
-      theFinalist.push(data[i])
-  }
-}
-  return theFinalist
+// function getFinals(data){
+//     const theFinalList = data => data.Stage === "Finals";
+//     const finalist = data.filter(theFinalList());
+//     return (finalist);
+// }
+// console.log(getFinals(fifaData))
 
-};
-console.log(getfinals(fifaData))
+// function getFinals(data) {
+//     const theFinalist = []
+//   for(let i =0; i <= data.length; i++){
+//   if( data[i].Stage === "Final" ){
+//       theFinalist.push(data[i])
+//   }
+// }
+//   return theFinalist
+
+// };
+// console.log(getFinals(fifaData))
+
+function theFinalList(data){
+let finalist= data => data.Stage ==="Final";
+}
+ console.log(theFinalList(fifaData))
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
 function getYears(/* code here */) {
