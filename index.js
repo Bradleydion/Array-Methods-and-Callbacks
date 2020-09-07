b48a75d3ea027a41ef63e8d6b99b03aaeb392885
Bradleydion <57144164+Bradleydion@users.noreply.github.com>
2020-09-06 22:10:41 -0400

    working on task 3. Need to put down for the night. I will work on this more in the morning

--- index.js
+++ index.js
@@ -44,15 +44,14 @@ console.log(getFinals(fifaData))
 // return (finalist)
 // }
 //  console.log(theFinalList(fifaData))
+
+
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
+function getYears(array) {
+const finalYears = getFinals(array).map();
 
-    /* code here */
-
-};
-
-getYears();
+console.log(getYears(fifaData));
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
